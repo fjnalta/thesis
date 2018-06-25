6d454586a2ab6dd799e4d8fd903c32a6f99408eb
Philipp Minges <fjnalta@gmail.com>
2018-06-25 19:12:47 +0200

    add TODO

--- src/CoAP_Server/index.js
+++ src/CoAP_Server/index.js
@@ -5,13 +5,15 @@ var coap = require('coap');
 var server = coap.createServer();
 
 server.on('request', function(req, res) {
-    var obj = JSON.parse(req.payload);
-    console.log('Title:' + obj.title);
-    console.log('Body:' + obj.body);
+    //var obj = JSON.parse(req.payload);
+    //console.log('Title:' + obj.title);
+    //console.log('Body:' + obj.body);
     res.end();
 });
 
 // the default CoAP port is 5683
 server.listen(function() {
-    console.log('Server listening on Port 5683');
+    //console.log('Server listening on Port 5683');
 });
+
+// TODO - z.B. Statuscode zurückliefern anhand von Temperatur
